4f089fb7d6dad71f5dca0265a7f3efaa144ce52a
Seth Samuel <Seths@missionreadyhq.com>
2025-06-13 14:34:55 +1200

    add and delete commands scafolded

--- dataSeeder.py
+++ dataSeeder.py
@@ -1,6 +1,7 @@
 # ------------------ IMPORTS AND SETUP ----------------- #
 
 import typer
+app = typer.Typer()
 
 # ----------------------- DEFINES ---------------------- #
 
@@ -9,8 +10,16 @@ import typer
 # --------------------- ENTRYPOINT --------------------- #
 
 
+@app.command("add")
+def add_data(data: str):
+    print(f"data added: {data}")
+
+@app.command("delete")
+def del_data(data: str):
+    print(f"data deleted: {data}")
+
 def main(name: str = "name" ):
     print(f"hello {name}")
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
