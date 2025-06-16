92b667fbac2c345eb282f043bda273eeca74ff3e
Seth Samuel <Seths@missionreadyhq.com>
2025-06-16 16:19:06 +1200

    cant get multi docs back

--- getFromDatabaseAPI/python/server.py
+++ getFromDatabaseAPI/python/server.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from fastapi.middleware.cors import CORSMiddleware 
 
 # ------------------------ SETUP ----------------------- #
 
@@ -14,13 +15,28 @@ mongoConnection = MongoClient(os.getenv("CONNECTION_STRING"))
 db = mongoConnection[os.getenv("DATABASE_NAME")] # type: ignore
 collection = db[os.getenv("COLLECTION_NAME")] # type: ignore
 
+# --------------------- MIDDLEWARES -------------------- #
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # ----------------------- ROUTES ----------------------- #
 
 
-app.get("/api/get")
+@app.get("/api/get")
 def get_from_mongo(key: str = "", value: str=""):
    print(f"key: {key} , value: {value}")
-   results =  collection.find({key: value})
-   return results
+   documentStore = []
+   for document in collection.find({key:value}):
+      documentStore.append(document)
+   return documentStore
+
+
+
+@app.get("/")
+def def_route():
+   return "helloworld"
