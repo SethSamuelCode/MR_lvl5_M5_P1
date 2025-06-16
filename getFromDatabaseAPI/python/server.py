ac8ce343f029d908fdcbe472a7a7a36a533c0855
Seth Samuel <Seths@missionreadyhq.com>
2025-06-16 18:42:36 +1200

    Refactor API endpoints to improve data retrieval and add JSON input handling

--- getFromDatabaseAPI/python/server.py
+++ getFromDatabaseAPI/python/server.py
@@ -5,6 +5,14 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 from fastapi.middleware.cors import CORSMiddleware 
+from pydantic import BaseModel
+import re
+
+# ----------------------- DEFINES ---------------------- #
+
+class RequestSchema(BaseModel):
+   key: str
+   value: str | int 
 
 # ------------------------ SETUP ----------------------- #
 
@@ -27,14 +35,26 @@ app.add_middleware(
 # ----------------------- ROUTES ----------------------- #
 
 
-@app.get("/api/get")
-async def get_from_mongo(key: str = "", value: str=""):
+@app.get("/api/get/{key}/{value}")
+async def get_from_mongo(key, value):
    print(f"key: {key} , value: {value}")
    documentStore = []
-   for document in collection.find({key : value}):
+   for document in collection.find({key: value}):
       document["_id"] = str(document["_id"])
       documentStore.append(document)
       print(document)
+   return documentStore
+
+@app.post("/api/getJson")
+async def get_from_mongo_json(input: RequestSchema):
+   key = input.key
+   value = input.value
+   print(f"key: {key} , value: {value}")
+   documentStore = []
+   for document in collection.find({key: value}):
+      document["_id"] = str(document["_id"])
+      documentStore.append(document)
+      # print(document)
    return documentStore
 
 @app.get("/api/getAll")
